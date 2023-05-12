205487dace886201f54102db4b40e149f317f486
Maximilian Stiller <maximilian.stiller1992@gmail.com>
2023-05-12 11:47:18 +0200

    fix: fix plane to fly correctly

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    //Public Variables
+    public GameObject plane;
+
+    //Private VAriables
+    private Vector3 offset = new(20, 2.5f, 12);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
